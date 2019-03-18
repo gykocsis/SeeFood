bbec8a3c9bc070393aecab4776b1a91b9d478f8c
Gyorgy Kocsis <gyorgykocsisestrea@gmail.com>
2019-03-18 21:27:24 +0100

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Kocsis György on 2019. 03. 18..
+//  Copyright © 2019. Jolakotturinn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
